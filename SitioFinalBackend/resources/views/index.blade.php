521353befa73b3b4fb43add9caa71d7886e143c5
manuugit <manuela.hernandezr@upb.edu.co>
2021-05-10 16:15:57 -0500

    plantilla inicial laravel

---
+++ SitioFinalBackend/resources/views/index.blade.php
@@ -0,0 +1,218 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>UNIVERSART</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Zoo Planet Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/main.js"></script>
+<script src="js/navegacion.js"></script>
+
+</head>
+<body>
+	<div id="boton_flecha" class= "ocultar_flecha">
+		<a href=#arriba>
+		<img src="images/flecha_up.png"> </a>
+	</div>
+		<!--header-->
+			<div class="header">
+				<div class="container">
+					<div class="header-top">
+						<nav class="navbar navbar-default">
+							<div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+								<div class="navbar-header">
+									  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+											<span class="sr-only">Toggle navigation</span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+									  </button>
+									<div class="navbar-brand">
+										<h1><a href="index.html">UNIVERSART</a></h1>
+									</div>
+								</div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							  <ul class="nav navbar-nav">
+									<li class="active"><a id="index.html" href="javascript:void(0)">Inicio <span class="sr-only">(current)</span></a></li>
+									<li><a id="about.html" href="javascript:void(0)">Acerca de</a></li>
+									<li><a id="services.html" href="javascript:void(0)">Artistas</a></li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Galería <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a id="gallery.html" href="javascript:void(0)">Arquitectura</a></li>
+									<li><a id="gallery2.html" href="javascript:void(0)">Pintura</a></li>
+									<li><a id="gallery3.html" href="javascript:void(0)">Escultura</a></li>
+								</ul>
+									</li>
+									<li><a id="login.html" href="javascript:void(0)">Registro</a></li>
+									<li><a id="contact.html" href="javascript:void(0)">API</a></li>
+								</ul>
+							</div> <!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+						</nav>
+
+					</div>
+				</div>
+			</div>
+			<a name="arriba">
+			<div class="header-banner">
+				<div class="container">
+					<div class="head-banner">
+						<h3>El rincón de las bellas artes, múltiples posibilidades</h3>
+						<p>El arte, como manifestación creativa de los pensamientos, emociones y sentimientos del hombre, tiene diversas formas de expresión (desde lo más abstracto a lo más natural). Podemos ver estas formas en las bellas artes: pintura, escultura, arquitectura, literatura, danza, música y como séptimo arte el cine.</p>
+					</div>
+					<div class="banner-grids">
+						<div class="col-md-6 banner-grid">
+							<h4>Iósif Stalin</h4>
+							<p>"El artista es el ingeniero del alma humana"</p>
+						</div>
+						<div class="col-md-6 banner-grid">
+						<h4>Edgar Degas</h4>
+							<p>"El arte no es lo que ves, sino lo que hace que otros vean"</p>
+						</div>
+						<div class="clearfix"></div> 
+					</div> 
+				</div> </a>
+			</div> 
+		<!--header-->
+			<!--welcome-->
+			<div class="content">
+				<div class="welcome">
+					<div class="container">
+						<h2>Algunas de las bellas artes</h2>
+							<div class="welcome-grids">
+								<div class="col-md-3 welcome-grid">
+									<img src="images/pintura1.jpg" alt=" " class="img-responsive" />
+									<div class="wel-info">
+										<h4>Pintura</h4>
+										<p>La pintura es una de las expresiones más antiguas de las bellas artes. Es una manifestación artística de las artes visuales, en la que podemos encontrar numerosas técnicas o materiales. En cada pintura se representan los valores estéticos del artista, emociones, conceptos, ideas, y posibles interpretaciones del mundo en todos sus ámbitos. </p>
+									</div>
+								</div>
+								<div class="col-md-3 welcome-grid">
+									<img src="images/escultura1.jpg" alt=" " class="img-responsive" />
+									<div class="wel-info">
+										<h4>Escultura</h4>
+										<p>Mediante la escultura es posible crear figuras tridimensionales esculpiendo sobre materiales sólidos. Es posible encontrar esculturas en madera, bronce, barro, arcilla, oro, plata, bronce e incluso materiales menos comunes como hielo y arena. Según la época podemos ver diferentes tipos: gótica, románica, griega, cinética y figurativa; desde lo más clásico a lo contemporáneo.</p>
+									</div>
+								</div>
+								<div class="col-md-3 welcome-grid">
+									<img src="images/arquitectura1.jpg" alt=" " class="img-responsive" />
+									<div class="wel-info">
+										<h4>Arquitectura</h4>
+										<p>Podemos ver la arquitectura como el arte de idear, diseñar y construir estructuras para la necesidad humana. Está en constante búsqueda de la funcionalidad, la perdurabilidad y el valor estético. En la arquitectura es importante que la belleza de la construcción alcance un equilibrio y armonía con la utilidad. Por seguir la estética, es considerada una de las bellas artes.</p>
+									</div>
+								</div>
+								<div class="col-md-3 welcome-grid">
+									<img src="images/danza1.jpg" alt=" " class="img-responsive" />
+									<div class="wel-info">
+										<h4>Danza</h4>
+										<p>La danza es una manifestación artística que transmite mediante expresiones corporales y movimientos al ritmo de la música; también es de las más antiguas de la Humanidad. En muchas culturas tiene un valor simbólico, ritual y representativo. Algunos de los elementos que componen una danza son el ritmo, el movimiento, la expresión corporal, el estilo y el espacio.</p>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+					</div>
+				</div>
+			<!--welcome-->
+						<!--feature-->
+
+				<!--feature-->
+				<!--animals-->
+					<div class="animals">
+						<div class="container">
+							<h3>Muestras artísticas</h3>
+							<div class="clients">
+								<ul id="flexiselDemo3">
+									<li><img src="images/pintura2.jpg" class="img-responsive" alt=""/></li>
+									<li><img src="images/arquitectura2.jpg" class="img-responsive" alt=""/></li>
+									<li><img src="images/escultura2.jpg" class="img-responsive" alt=""/></li>
+									<li><img src="images/danza2.jpg" class="img-responsive" alt=""/></li>
+									<li><img src="images/arquitectura3.jpg" class="img-responsive" alt=""/></li>
+									<li><img src="images/pintura2.jpg" class="img-responsive" alt=""/></li>
+									<li><img src="images/arquitectura2.jpg" class="img-responsive" alt=""/></li>
+									<li><img src="images/escultura2.jpg" class="img-responsive" alt=""/></li>
+									<li><img src="images/danza2.jpg" class="img-responsive" alt=""/></li>
+									<li><img src="images/arquitectura3.jpg" class="img-responsive" alt=""/></li>
+								</ul>
+									<script type="text/javascript">
+								$(window).load(function() {
+									
+								  $("#flexiselDemo3").flexisel({
+										visibleItems: 5,
+										animationSpeed: 1000,
+										autoPlay: true,
+										autoPlaySpeed: 3000,    		
+										pauseOnHover: true,
+										enableResponsiveBreakpoints: true,
+										responsiveBreakpoints: { 
+											portrait: { 
+												changePoint:480,
+												visibleItems: 1
+											}, 
+											landscape: { 
+												changePoint:640,
+												visibleItems: 2
+											},
+											tablet: { 
+												changePoint:768,
+												visibleItems: 3
+											}
+										}
+									});
+									});
+								</script>
+								<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+							<div class="clearfix"></div>
+						</div>
+					</div>
+				</div>
+			<!--models-->
+			<!--testimonials-->
+			<div class="testimonials">
+				<div class="container">
+					<h3>Expresión</h3>
+					<p>El arte no es una forma de ganarse la vida. Es más bien una forma muy humana de hacer la vida más soportable. Practicar un arte, bien o mal, es una forma de hacer crecer el alma. Por el amor de Dios, canten en la ducha. Bailen con la música de la radio. Cuenten cuentos. Escriban un poema para un amigo o para una amiga, aunque sea pésimo. Háganlo tan bien como sepan y obtendrán una enorme recompensa. Habrán creado algo.</p>
+					<h4>Kurt Vonnegut Jr</h4>
+				</div>
+			</div>
+			<!--testimonials-->
+
+						<!--events-->
+							
+						<!--events-->
+						<!--specials-->
+				<div class="specials-section">
+					<div class="container">
+						<div class="specials-grids">
+					</div>
+				</div>
+			</div>
+
+			<!--footer-->
+			<div class="footer-section">
+				<div class="container">
+					<div class="footer-top">
+						<p>Esteban González Gómez | Email : <a href="mailto:esteban.gonzalezg@upb.edu.co">esteban.gonzalezg@upb.edu.co</a></p>
+						<p>Manuela Hernández Ríos | Email : <a href="mailto:manuela.hernandezr@upb.edu.co">manuela.hernandezr@upb.edu.co</a></p> <br>
+						<p>&copy; 2021 UNIVERSART . All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+					</div>
+					<div id="boton_wpp"><a href="https://wa.me/573007883256"><img src="https://cdn.icon-icons.com/icons2/373/PNG/256/Whatsapp_37229.png" width="72px" height="72px"></a></div>
+				</div>
+			</div>
+</body>
+</html>
