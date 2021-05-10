756e73409e1a79ab7a46481b44f62864e61b7595
manuugit <manuela.hernandezr@upb.edu.co>
2021-05-10 16:35:02 -0500

    agrego vista galeria 3

---
+++ SitioFinalBackend/resources/views/gallery3.blade.php
@@ -0,0 +1,161 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>UNIVERSART</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Zoo Planet Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/navegacion.js"></script>
+<!--lightbox-->
+<link rel="stylesheet" type="text/css" href="css/jquery.lightbox.css">
+<script src="js/jquery.lightbox.js"></script>
+<script>
+  // Initiate Lightbox
+  $(function() {
+    $('.gallery a').lightbox(); 
+  });
+</script>
+<!--lightbox-->
+</head>
+<body>
+		<!--header-->
+			<div class="header">
+				<div class="container">
+					<div class="header-top">
+						<nav class="navbar navbar-default">
+							<div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+								<div class="navbar-header">
+									  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+											<span class="sr-only">Toggle navigation</span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+									  </button>
+									<div class="navbar-brand">
+										<h1><a href="index.html">UNIVERSART</a></h1>
+									</div>
+								</div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav">
+									<li><a id="index.html" href="javascript:void(0)">Inicio <span class="sr-only">(current)</span></a></li>
+									<li><a id="about.html" href="javascript:void(0)">Acerca de</a></li>
+									<li><a id="services.html" href="javascript:void(0)">Artistas</a></li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Galería <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a id="gallery.html" href="javascript:void(0)">Arquitectura</a></li>
+									<li><a id="gallery2.html" href="javascript:void(0)">Pintura</a></li>
+									<li class="active"><a id="gallery3.html" href="javascript:void(0)">Escultura</a></li>
+								</ul>
+									</li>
+									<li><a id="login.html" href="javascript:void(0)">Registro</a></li>
+									<li><a id="contact.html" href="javascript:void(0)">API</a></li>
+								</ul>
+								
+							</div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+						</nav>
+
+					</div>
+				</div>
+			</div>
+		<div class="banner-header">
+			<div class="container">
+				<h2>Escultura</h2>
+			</div>
+			</div>
+	<div class="content">
+	<!--gallery-->
+			<div class="gallery-section">
+					<div class="container">
+					<div class="gallery-grids">
+				<div class="col-md-4 gallery-grid">
+					<div class="gallery">
+						<a class="mask" href="images/esculturaCupido.jpg"><img src="images/esculturaCupido.jpg" class="img-responsive zoom-img" alt="/" title="image-name"></a>
+					</div>	
+				</div>
+				<div class="col-md-4 gallery-grid">
+					<div class="gallery">
+						<a class="mask" href="images/esculturaDante.jpg"><img src="images/esculturaDante.jpg" class="img-responsive zoom-img" alt="/" title="image-name"></a>
+					</div>	
+				</div>
+				<div class="col-md-4 gallery-grid">
+					<div class="gallery">
+						<a class="mask" href="images/esculturaFrancia.jpg"><img src="images/esculturaFrancia.jpg" class="img-responsive zoom-img" alt="/" title="image-name"></a>
+					</div>	
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="gallery-grids">
+				<div class="col-md-4 gallery-grid">
+					<div class="gallery">
+						<a class="mask" href="images/esculturaFuente.jpg"><img src="images/esculturaFuente.jpg" class="img-responsive zoom-img" alt="/" title="image-name"></a>
+					</div>	
+				</div>
+				<div class="col-md-4 gallery-grid">
+					<div class="gallery">
+						<a class="mask" href="images/esculturaHercules.jpg"><img src="images/esculturaHercules.jpg" class="img-responsive zoom-img" alt="/" title="image-name"></a>
+					</div>	
+				</div>
+				<div class="col-md-4 gallery-grid">
+					<div class="gallery">
+						<a class="mask" href="images/esculturaMedusa.jpg"><img src="images/esculturaMedusa.jpg" class="img-responsive zoom-img" alt="/" title="image-name"></a>
+					</div>	
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="gallery-grids">
+				<div class="col-md-4 gallery-grid">
+					<div class="gallery">
+							<a class="mask" href="images/esculturaMichelAngelo.jpg"><img src="images/esculturaMichelAngelo.jpg" class="img-responsive zoom-img" alt="/" title="image-name"></a>
+						</div>	
+				</div>
+				<div class="col-md-4 gallery-grid">
+					<div class="gallery">
+							<a class="mask" href="images/esculturaMujer.jpg"><img src="images/esculturaMujer.jpg" class="img-responsive zoom-img" alt="/" title="image-name"></a>
+						</div>	
+				</div>
+				<div class="col-md-4 gallery-grid">
+					<div class="gallery">
+							<a class="mask" href="images/esculturaZeus.jpg"><img src="images/esculturaZeus.jpg" class="img-responsive zoom-img" alt="/" title="image-name"></a>
+						</div>	
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+<!--gallery-->
+<!--specials-->
+				<div class="specials-section">
+					<div class="container">
+						<div class="specials-grids">
+					</div>
+				</div>
+			</div>
+			<!--footer-->
+			<div class="footer-section">
+				<div class="container">
+					<div class="footer-top">
+						<p>Esteban González Gómez | Email : <a href="mailto:esteban.gonzalezg@upb.edu.co">esteban.gonzalezg@upb.edu.co</a></p>
+						<p>Manuela Hernández Ríos | Email : <a href="mailto:manuela.hernandezr@upb.edu.co">manuela.hernandezr@upb.edu.co</a></p> <br>
+						<p>&copy; 2021 UNIVERSART . All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+					</div>
+				</div>
+			</div>
+</body>
+</html>
