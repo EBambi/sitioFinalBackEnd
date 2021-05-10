67c517766f28ce4be608c3250ddc12e9db10299a
EBambi <esteban.gonzalezg@upb.edu.co>
2021-05-10 16:32:36 -0500

    contact

---
+++ SitioFinalBackend/resources/views/contact.blade.php
@@ -0,0 +1,102 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>UNIVERSART</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Zoo Planet Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/api.js"></script>
+</head>
+<body>
+		<!--header-->
+			<div class="header">
+				<div class="container">
+					<div class="header-top">
+						<nav class="navbar navbar-default">
+							<div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+								<div class="navbar-header">
+									  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+											<span class="sr-only">Toggle navigation</span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+									  </button>
+									<div class="navbar-brand">
+										<h1><a href="index.html">UNIVERSART</a></h1>
+									</div>
+								</div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav">
+									<li><a id="index.html" href="javascript:void(0)">Inicio <span class="sr-only">(current)</span></a></li>
+									<li><a id="about.html" href="javascript:void(0)">Acerca de</a></li>
+									<li><a id="services.html" href="javascript:void(0)">Artistas</a></li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Galería <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a id="gallery.html" href="javascript:void(0)">Arquitectura</a></li>
+									<li><a id="gallery2.html" href="javascript:void(0)">Pintura</a></li>
+									<li><a id="gallery3.html" href="javascript:void(0)">Escultura</a></li>
+								</ul>
+									</li>
+									<li><a id="login.html" href="javascript:void(0)">Registro</a></li>
+									<li class="active"><a id="contact.html" href="javascript:void(0)">API</a></li>
+								</ul>
+								
+							</div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+						</nav>
+
+					</div>
+				</div>
+				</div>
+					
+		<div class="banner-header">
+			<div class="container">
+				<h2>Servicio Externo API</h2>
+			</div>
+			</div>
+	<!--header-->
+	 <div class="content">
+ 		<div class="contact">
+ 			<div class="container">
+				 	<div id="resultado" class="grid_parent"></div>
+		 			<div class="contact-map">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d424396.3176723366!2d150.92243255000002!3d-33.7969235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b129838f39a743f%3A0x3017d681632a850!2sSydney+NSW%2C+Australia!5e0!3m2!1sen!2sin!4v1431587453420"></iframe>
+					</div>	
+			</div>
+		</div>
+	</div>
+	<!--specials-->
+				<div class="specials-section">
+					<div class="container">
+						<div class="specials-grids">
+					</div>
+				</div>
+			</div>
+			<!--footer-->
+			<div class="footer-section">
+				<div class="container">
+					<div class="footer-top">
+						<p>Esteban González Gómez | Email : <a href="mailto:esteban.gonzalezg@upb.edu.co">esteban.gonzalezg@upb.edu.co</a></p>
+						<p>Manuela Hernández Ríos | Email : <a href="mailto:manuela.hernandezr@upb.edu.co">manuela.hernandezr@upb.edu.co</a></p> <br>
+						<p>&copy; 2021 UNIVERSART . All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+					</div>
+				</div>
+			</div>
+</body>
+</html>
