9dc41363708d65fe057602e51d1082980171cf14
EBambi <esteban.gonzalezg@upb.edu.co>
2021-05-10 16:30:51 -0500

    about

---
+++ SitioFinalBackend/resources/views/about.blade.php
@@ -0,0 +1,146 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>UNIVERSART</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Zoo Planet Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/navegacion.js"></script>
+</head>
+<body>
+		<!--header-->
+			<div class="header">
+				<div class="container">
+					<div class="header-top">
+						<nav class="navbar navbar-default">
+							<div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+								<div class="navbar-header">
+									  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+											<span class="sr-only">Toggle navigation</span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+									  </button>
+									<div class="navbar-brand">
+										<h1><a href="index.html">UNIVERSART</a></h1>
+									</div>
+								</div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav">
+									<li><a id="index.html" href="javascript:void(0)">Inicio <span class="sr-only">(current)</span></a></li>
+									<li class="active"><a id="about.html" href="javascript:void(0)">Acerca de</a></li>
+									<li><a id="services.html" href="javascript:void(0)">Artistas</a></li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Galería <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a id="gallery.html" href="javascript:void(0)">Arquitectura</a></li>
+									<li><a id="gallery2.html" href="javascript:void(0)">Pintura</a></li>
+									<li><a id="gallery3.html" href="javascript:void(0)">Escultura</a></li>
+								</ul>
+									</li>
+									<li><a id="login.html" href="javascript:void(0)">Registro</a></li>
+									<li><a id="contact.html" href="javascript:void(0)">API</a></li>
+								</ul>
+								
+							</div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+						</nav>
+
+					</div>
+				</div>
+				</div>	
+		<div class="banner-header">
+			<div class="container">
+				<h2>Acerca de</h2>
+			</div>
+			</div>
+	<!--about-->
+	<div class="content">
+			<div class="about-section">
+				<div class="container">
+						<div class="about-grids">
+							<div class="col-md-4 about-grid">
+								<h3>¿Quiénes somos?</h3>
+								<img src="images/logoUPB.jpg" class="img-responsive" alt="/">
+								<div class="about1">
+									<h4>UPB</h4>
+									<p>La UPB es una institución caracterizada por su gran contribución en la formación integral para la transformación social y humana, también cuenta con una acreditación de alta calidad multicampus gracias a sus sedes en Medellín, Bogotá, Bucaramanga, Montería y Palmira.</p>
+								</div>
+							</div>
+							<div class="col-md-4 about-grid">
+							<h3>¿Por qué lo hacemos?</h3>
+								<div class="history">
+									<h4>Plataformas de Programación Empresarial</h4>
+									<p>Este sitio web es creado para la materia Plataformas de Programación como proyecto final, lo que se busca es crear el sitio con los parámetros dados por el profesor como tomar una plantilla de <a href="https://w3layouts.com">w3layouts</a>, crear una navegación centralizada, entre otros y posteriormente configurar un servidor en AWS con el sitio terminado.</p>
+								</div>
+							</div>
+							<div class="col-md-4 about-grid">
+							<h3>¿Quiénes lo Hacemos</h3>
+								<div class="trend">
+									<h4>Desarrolladores</h4>
+									<p>Los desarrolladores de este proyecto final son estudiantes de la UPB, ambos cursando 5to semestre.</p>
+								</div>
+								<div class="trend">
+									<ul>
+									<li>- Esteban González Gómez</li>
+									<li>- Manuela Hernández Ríos</li>
+									</ul>
+								</div>
+							</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+			</div>
+		<!--about-->
+				<div class="ourteam">
+					<div class="container">
+						<h3>Equipo</h3>
+						<div class="team-grids">
+						<div class="col-md-3 team-grid">
+							<img src="images/fotoEsteban.png" class="img-responsive" alt="" />
+							<h4>Esteban González Gómez</h4>
+							<p>Ingeniería de Sistemas e Informática</p>
+						</div>
+						<div class="col-md-3 team-grid">
+							<img src="images/foto.png" class="img-responsive" alt="" />
+							<h4>Manuela Hernández Ríos</h4>
+							<p>Ingeniería de Sistemas e Informática</p>
+						</div>
+					<div class="clearfix"></div>
+					</div>
+				</div>
+			</div>
+			<!--specials-->
+				<div class="specials-section">
+					<div class="container">
+						<div class="specials-grids">
+					</div>
+				</div>
+			</div>
+			<!--footer-->
+			<div class="footer-section">
+				<div class="container">
+					<div class="footer-top">
+						<p>Esteban González Gómez | Email : <a href="mailto:esteban.gonzalezg@upb.edu.co">esteban.gonzalezg@upb.edu.co</a></p>
+						<p>Manuela Hernández Ríos | Email : <a href="mailto:manuela.hernandezr@upb.edu.co">manuela.hernandezr@upb.edu.co</a></p> <br>
+						<p>&copy; 2021 UNIVERSART . All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+					</div>
+				</div>
+			</div>
+</body>
+</html>
